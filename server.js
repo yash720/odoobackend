6b851c5a1c659a1ff46b5c8345ca5fc44b88f440
yash720 <66953230+yash720@users.noreply.github.com>
2025-07-12 17:44:46 +0530

    Update server.js

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(helmet());
 
 // CORS configuration
 app.use(cors({
-  origin: process.env.CORS_ORIGIN || 'http://localhost:5173',
+  origin: process.env.CORS_ORIGIN || 'odoofrontend.vercel.app',
   credentials: true
 }));
 
